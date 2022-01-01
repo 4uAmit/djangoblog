2bcbe40b92b2fa0a30eaefe4d07c9a6097b8805a
4uAmit <95918940+4uAmit@users.noreply.github.com>
2022-01-01 18:05:08 +0530

    changes

--- newblog/settings.py
+++ newblog/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 WHITENOISE_USE_FINDERS=True
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
